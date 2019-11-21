0e5715945abe939c129d0d82f28360b7a47778d6
nancylee713 <srlee713@gmail.com>
2019-11-21 09:15:35 -0700

    Add favorites table

--- db/migrations/20191120190404_user.js
+++ db/migrations/20191120190404_user.js
@@ -1,20 +1,28 @@
-
 exports.up = function(knex) {
   return Promise.all([
-    knex.schema.createTable('users', function (table) {
-      table.increments('id').primary();
-      table.string('email');
-      table.string('password');
-      table.string('apiKey');
-      table.string('favoriteLocation');
+    knex.schema.createTable("users", function(table) {
+      table.increments("id").primary();
+      table.string("email");
+      table.string("password");
+      table.string("apiKey");
+
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable("favorites", function(table) {
+      table.increments("id").primary();
+      table.string("city");
+      table.integer("user_id").unsigned();
+      table.foreign("user_id").references("users.id");
 
       table.timestamps(true, true);
     })
-  ])
+  ]);
 };
 
 exports.down = function(knex) {
   return Promise.all([
-    knex.schema.dropTable('users')
+    knex.schema.dropTable("users"),
+    knex.schema.dropTable("favorites")
   ]);
 };
