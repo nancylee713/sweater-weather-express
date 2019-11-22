22f005db413787c46af2ebe82ee730ba3639a68c
nancylee713 <srlee713@gmail.com>
2019-11-21 21:51:31 -0700

    Import config to forecast

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('../../../knexfile')[environment];
-const database = require('knex')(configuration);
-
+const database = require('../../../config');
 const helper = require("../../../helpers/api_helper");
 const formatData = helper.formatData;
 
